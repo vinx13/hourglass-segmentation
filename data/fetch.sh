998f4910d5b12f35d4360597222929140993489f
Wuwei Lin <vincentl13x@gmail.com>
2017-02-25 13:50:36 +0800

    Fix scripts

--- data/fetch.sh
+++ data/fetch.sh
@@ -2,6 +2,6 @@
 wget http://www.stat.ucla.edu/~xianjie.chen/pascal_part_dataset/trainval.tar.gz
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2010/VOCtrainval_03-May-2010.tar
 tar -xzf trainval.tar.gz Annotations_Part
-tar -xzf VOCtrainval_03-May-2010.tar
+tar -xf VOCtrainval_03-May-2010.tar
 
 python convert.py
